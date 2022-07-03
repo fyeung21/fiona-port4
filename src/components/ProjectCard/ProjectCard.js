167fd53d485455da6da954f9ba730b5911e389da
Fiona Y <fyeung21@gmail.com>
2022-07-02 19:34:40 -0700

    Add cta to navigate to single project

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,18 +1,24 @@
-import ShortStack from "./ShortStack";
+import { Link } from 'react-router-dom';
+import ShortStack from './ShortStack';
 
 const ProjectCard = () => {
-    return (
-        <article>
-            <div className='card-img-container'>
-                <img src={''} alt={"project thumbnail"} />
-            </div>
-            <div className='card-text-container'>
-                <h2>{"project title"}</h2>
-                <p>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium, augue ac facilisis mattis, dui lorem dictum libero, quis imperdiet lorem dui sit amet lorem."}</p>
-                <ShortStack/>
-            </div>
-        </article>
-    )
-}
+  return (
+    <article>
+      <div className="card-img-container">
+        <img src={''} alt={'project thumbnail'} />
+      </div>
+      <div className="card-text-container">
+        <h2>{'project title'}</h2>
+        <p>
+          {
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium, augue ac facilisis mattis, dui lorem dictum libero '
+          }
+          <Link to={`/works/${'workSingle'}`}>...more</Link>
+        </p>
+        <ShortStack />
+      </div>
+    </article>
+  );
+};
 
 export default ProjectCard;
