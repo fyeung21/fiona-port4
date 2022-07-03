899516694ed6ebbb75e31e7ffcc69dc5e004ca0b
Fiona Y <fyeung21@gmail.com>
2022-07-03 16:23:51 -0700

    Add title to project nav

--- src/components/ProjectNav/ProjectNav.js
+++ src/components/ProjectNav/ProjectNav.js
@@ -3,6 +3,7 @@ import NavCard from './NavCard';
 const ProjectNav = () => {
   return (
     <div class="project-nav-container">
+      <h3>more projects</h3>
       <NavCard />
       <NavCard />
       <NavCard />
