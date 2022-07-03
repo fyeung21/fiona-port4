daa2d42de81dcbaf758e4a7d41fae2b6a379df80
Fiona Y <fyeung21@gmail.com>
2022-07-02 20:53:07 -0700

    Add 404 components

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
     return (
         <>
             <h2>This is the 404 page</h2>
+            <p>Oops! You have reached the 404 page.</p>
+            <Link to="/work">Back to Home</Link>
         </>
     )
 }
