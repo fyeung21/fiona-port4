73a58c66418e5164a67ec8e8919a3e45d4ff2a40
Fiona Y <fyeung21@gmail.com>
2022-07-06 01:19:00 -0700

    Set up api call for single work

--- src/components/MainProjects/MainProjects.js
+++ src/components/MainProjects/MainProjects.js
@@ -1,16 +1,44 @@
-import ProjectCard from "../ProjectCard/ProjectCard";
+import { useState, useEffect } from 'react';
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 const MainProjects = () => {
-    return (
-        <section className='main-projects-container'>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
-        </section>
-    )
-}
+  const { id } = useParams();
+  const restPath = `https://fionayeung.site/wp-portfolio/wp-json/wp/v2/fio-project/${id}?acf_format=standard`;
+  const [restData, setData] = useState([]);
+  const [isLoaded, setLoadStatus] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(restPath);
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
+    };
+    fetchData();
+  }, [restPath]);
+
+  return (
+    <>
+      {isLoaded ? (
+        <>
+          <section className="main-projects-container">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </section>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
+};
 
 export default MainProjects;
--- src/pages/PageSingleWork.js
+++ src/pages/PageSingleWork.js
@@ -3,12 +3,39 @@ import BannerImg from '../components/SingleWork/BannerImg';
 import Stats from '../components/SingleWork/Stats';
 
 const PageSingleWork = () => {
+  const { id } = useParams();
+  const restPath = `https://fionayeung.site/wp-portfolio/wp-json/wp/v2/fio-project/${id}?acf_format=standard`;
+  const [restData, setData] = useState([]);
+  const [isLoaded, setLoadStatus] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(restPath);
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
+    };
+    fetchData();
+  }, [restPath]);
+
   return (
-    <section className='page-single-work'>
-      <BannerImg />
-      <Stats />
-      <ProjectNav />
-    </section>
+    <>
+      {isLoaded ? (
+        <>
+          <section className="page-single-work">
+            <BannerImg />
+            <Stats />
+            <ProjectNav />
+          </section>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
   );
 };
 
