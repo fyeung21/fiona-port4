e5ba3497757245a55c33d4371dc91aec0cad48db
Fiona Y <fyeung21@gmail.com>
2022-07-06 23:19:38 -0700

    Edit project card styles

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,20 +4,23 @@ import Placeholder from '../coffee-wireframes.jpg';
 
 const ProjectCard = () => {
   return (
-    <article className='project-card-container'>
-      <div className="card-img-container">
-        <img src={Placeholder} alt={'project thumbnail'} />
-      </div>
-      <div className="card-text-container">
-        <h2>{'project title'}</h2>
-        <p>
-          {
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium, augue ac facilisis mattis, dui lorem dictum libero '
-          }
-          <Link to={`/works/${'workSingle'}`}>...more</Link>
-        </p>
-        <ShortStack />
-      </div>
+    <article className="project-card-container">
+      <Link to={`/works/${'workSingle'}`}>
+        <div>
+          <section className="card-img-container">
+            <img src={Placeholder} alt={'project thumbnail'} />
+          </section>
+          <section className="card-text-container">
+            <h2>{'project title'}</h2>
+            <p>
+              {
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium, augue ac facilisis mattis, dui lorem dictum libero '
+              }...see more
+            </p>
+            <ShortStack />
+          </section>
+        </div>
+      </Link>
     </article>
   );
 };
