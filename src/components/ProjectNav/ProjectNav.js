bbb9a2141d9d9eba0dd9b53ebe0df594d2004c80
Fiona Y <fyeung21@gmail.com>
2022-07-07 11:07:30 -0700

    Add API call to project nav

--- src/components/ProjectNav/ProjectNav.js
+++ src/components/ProjectNav/ProjectNav.js
@@ -1,32 +1,43 @@
+import { useState, useEffect } from 'react';
+import {useParams} from 'react-router-dom';
 import NavCard from './NavCard';
 import Placeholder from '../coffee-wireframes.jpg';
 
 const ProjectNav = () => {
+  const { slug } = useParams();
+  const restPath = `https://fiona-yeung.com/wp-portfolio/wp-json/wp/v2/fio-project?acf_format=standard&slug=${slug}&_embed`;
+  const [restData, setData] = useState([]);
+  const [isLoaded, setLoadStatus] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(restPath);
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
+    };
+    fetchData();
+  }, [restPath]);
+
   return (
     <>
-      <h3>more projects</h3>
-      <div className="project-nav-container">
-        <NavCard 
-          thumbnail={Placeholder}
-          alt={"Placeholder"}
-          projectTitle={"Project Title"}
-          />
-        <NavCard 
-          thumbnail={Placeholder}
-          alt={"Placeholder"}
-          projectTitle={"Project Title"}
-          />
-        <NavCard 
-          thumbnail={Placeholder}
-          alt={"Placeholder"}
-          projectTitle={"Project Title"}
-          />
-          <NavCard 
-          thumbnail={Placeholder}
-          alt={"Placeholder"}
-          projectTitle={"Project Title"}
-          />
-      </div>
+      {isLoaded ? (
+        <>
+          <h3>more projects</h3>
+          <div className="project-nav-container">
+            <NavCard thumbnail={Placeholder} alt={'Placeholder'} projectTitle={'Project Title'} />
+            <NavCard thumbnail={Placeholder} alt={'Placeholder'} projectTitle={'Project Title'} />
+            <NavCard thumbnail={Placeholder} alt={'Placeholder'} projectTitle={'Project Title'} />
+            <NavCard thumbnail={Placeholder} alt={'Placeholder'} projectTitle={'Project Title'} />
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 };
