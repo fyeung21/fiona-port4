5647f97f8a1ce3dea912f74058e46b768ddc460e
Fiona Y <fyeung21@gmail.com>
2022-07-08 23:42:55 -0700

    Add htaccess file and code to run on server

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -8,7 +8,7 @@ import Footer from '../components/Footer/Footer';
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <Header />
       <div className="wrapper">
         <main>
