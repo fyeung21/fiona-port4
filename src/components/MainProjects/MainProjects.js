289fc7b9be0f9a964a7b336880fc0f8c19902e37
Fiona Y <fyeung21@gmail.com>
2022-07-14 15:50:02 -0700

    Add Animate on Scroll function to main projects

--- src/components/MainProjects/MainProjects.js
+++ src/components/MainProjects/MainProjects.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import ProjectCard from '../ProjectCard/ProjectCard';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 const MainProjects = ({ projectArr }) => {
   const restPath = `https://fiona-yeung.com/wp-portfolio/wp-json/wp/v2/fio-project?acf_format=standard&include=${projectArr}&orderby=include&_embed&v=4`;
@@ -20,11 +22,17 @@ const MainProjects = ({ projectArr }) => {
     fetchData();
   }, [restPath]);
 
+  useEffect(() => {
+    AOS.init({
+      duration : 2000
+    });
+  }, []);
+
   return (
     <>
       {isLoaded ? (
         <>
-          <section className="main-projects-container">
+          <section className="main-projects-container" data-aos="fade-left">
             {restData.map((item, id) => (
               <ProjectCard
                 key={id}
