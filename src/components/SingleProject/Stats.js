879aa7fa47a676e26258aade72b887af50e3be0f
Fiona Y <fyeung21@gmail.com>
2023-01-25 04:29:36 -0800

    Restyle project summary stats

--- src/components/SingleProject/Stats.js
+++ src/components/SingleProject/Stats.js
@@ -4,60 +4,64 @@ const Stats = ({ title, brief, liveLink, githubLink, platforms, roles, technolog
       <section className="stats-container">
         <div className="title-container">
           <h2>{title}</h2>
-          <article className="cta-container">
-            {liveLink !== '' ? (
-              <a
-                href={liveLink}
-                title="link to live site"
-                rel="noreferrer noopener"
-                target="_blank">
-                live site
-              </a>
-            ) : (
-              ''
-            )}
-            {githubLink !== '' ? (
-              <a
-                href={githubLink}
-                title="link to github repo"
-                rel="noreferrer noopener"
-                target="_blank">
-                view github
-              </a>
-            ) : (
-              ''
-            )}
-          </article>
         </div>
-        <article>
-          <h3>brief</h3>
-          <p>{brief}</p>
-        </article>
-        <div className="flex-container">
-        <article>
-          <h3>platforms</h3>
-          <ul>
-            {platforms.map((item, id) => (
-              <li key={id}>{item}</li>
-            ))}
-          </ul>
-        </article>
-        <article>
-          <h3>my roles</h3>
-          <ul>
-            {roles.map((item, id) => (
-              <li key={id}>{item}</li>
-            ))}
-          </ul>
-        </article>
-        <article>
-          <h3>technologies</h3>
-          <ul>
-            {technologies.map((item, id) => (
-              <li key={id}>{item}</li>
-            ))}
-          </ul>
-        </article>
+        <div className="summary-flex-container">
+          <section className="primary-summary-container">
+            <article className="brief-container">
+              <h3>brief</h3>
+              <p>{brief}</p>
+            </article>
+            <article className="cta-container">
+              {liveLink !== '' ? (
+                <a
+                  href={liveLink}
+                  title="link to live site"
+                  rel="noreferrer noopener"
+                  target="_blank">
+                  live site
+                </a>
+              ) : (
+                ''
+              )}
+              {githubLink !== '' ? (
+                <a
+                  href={githubLink}
+                  title="link to github repo"
+                  rel="noreferrer noopener"
+                  target="_blank">
+                  view github
+                </a>
+              ) : (
+                ''
+              )}
+            </article>
+          </section>
+          <section className="secondary-summary-container">
+            <article>
+              <h3>platforms</h3>
+              <ul>
+                {platforms.map((item, id) => (
+                  <li key={id}>{item}</li>
+                ))}
+              </ul>
+            </article>
+            <article className="my-roles">
+              <h3>my roles</h3>
+              <ul>
+                {roles.map((item, id) => (
+                  <li key={id}>{item}</li>
+                ))}
+              </ul>
+            </article>
+            <article>
+              <h3>technologies</h3>
+              <ul>
+                {technologies.map((item, id) => (
+                  <li key={id}>{item}</li>
+                ))}
+              </ul>
+            </article>
+          </section>
         </div>
       </section>
     </>
