27d8bf481e6cfdd41541f8e609b4e7c2c8210330
Fiona Y <fyeung21@gmail.com>
2023-03-21 14:29:47 -0700

    Fix bug where restData was not targetting the correct object for footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -27,13 +27,12 @@ const Footer = () => {
       {isLoaded ? (
         <footer>
           <div className="footer-container">
-            <cite>&copy; {restData[0].acf.copyright_year} Fiona Yeung</cite>
+            <cite>&copy; {restData[1].acf.copyright_year} Fiona Yeung</cite>
             <div>
-              {/* <p>{restData[0].acf.footer_message}</p> */}
               <ul>
                 <li>
                   <a
-                    href={`mailto:${restData[0].acf.email_link}`}
+                    href={`mailto:${restData[1].acf.email_link}`}
                     title="Link to Email"
                     rel="noreferrer noopener">
                     <Email/>
@@ -41,16 +40,16 @@ const Footer = () => {
                 </li>
                 <li>
                   <a
-                    href={restData[0].acf.linkedin_link.url}
-                    title={"Link to " + restData[0].acf.linkedin_link.title}
+                    href={restData[1].acf.linkedin_link.url}
+                    title={"Link to " + restData[1].acf.linkedin_link.title}
                     rel="noreferrer noopener">
                     <Linkedin/>
                   </a>
                 </li>
                 <li>
                   <a
-                    href={restData[0].acf.github_link.url}
-                    title={"Link to " + restData[0].acf.github_link.title}
+                    href={restData[1].acf.github_link.url}
+                    title={"Link to " + restData[1].acf.github_link.title}
                     rel="noreferrer noopener">
                       <Github/>
                   </a>
